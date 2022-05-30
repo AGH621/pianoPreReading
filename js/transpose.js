246e2f7d8a756f0fc4dca0db98f6be160810e617
Anne <anne@mfm.org>
2022-05-30 16:42:52 -0700

    Retro-fit the class pitch class names to identify individual pitches.  This allows for transposing with the JS script

--- js/transpose.js
+++ js/transpose.js
@@ -4,6 +4,9 @@ function black_transpose() {
     let the_fs = document.getElementsByClassName("f")
     let the_gs = document.getElementsByClassName("g")
     let the_as = document.getElementsByClassName("a")
+    let the_fsharps = document.getElementsByClassName("fsharp")
+    let the_gsharps = document.getElementsByClassName("gsharp")
+    let the_asharps = document.getElementsByClassName("asharp")
 
     for (let i=0; i<the_fs.length; i++) {
         the_fs[i].src = "../../images/pngs/single_f_sharp.png"
@@ -16,22 +19,46 @@ function black_transpose() {
     for (let i=0; i<the_as.length; i++) {
         the_as[i].src = "../../images/pngs/single_a_sharp.png"
     }
+    for (let i=0; i<the_fsharps.length; i++) {
+        the_fsharps[i].src = "../../images/pngs/single_f_sharp.png"
+    }
+
+    for (let i=0; i<the_gsharps.length; i++) {
+        the_gsharps[i].src = "../../images/pngs/single_g_sharp.png"
+    }
+
+    for (let i=0; i<the_asharps.length; i++) {
+        the_asharps[i].src = "../../images/pngs/single_a_sharp.png"
+    }
 }
 
 function white_transpose() {
     let the_fs = document.getElementsByClassName("f")
     let the_gs = document.getElementsByClassName("g")
     let the_as = document.getElementsByClassName("a")
+    let the_fsharps = document.getElementsByClassName("fsharp")
+    let the_gsharps = document.getElementsByClassName("gsharp")
+    let the_asharps = document.getElementsByClassName("asharp")
 
     for (let i=0; i<the_fs.length; i++) {
         the_fs[i].src = "../../images/pngs/single_f.png"
     }
-
     for (let i=0; i<the_gs.length; i++) {
         the_gs[i].src = "../../images/pngs/single_g.png"
     }
 
     for (let i=0; i<the_as.length; i++) {
         the_as[i].src = "../../images/pngs/single_a.png"
+    }
+    for (let i=0; i<the_fsharps.length; i++) {
+        the_fsharps[i].src = "../../images/pngs/single_f.png"
+    }
+
+    for (let i=0; i<the_gsharps.length; i++) {
+        the_gsharps[i].src = "../../images/pngs/single_g.png"
+    }
+
+    for (let i=0; i<the_asharps.length; i++) {
+        the_asharps[i].src = "../../images/pngs/single_a.png"
     }
 }
