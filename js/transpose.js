29a46993f4e94fa24fd8f44adf9b131344472430
Anne <anne@mfm.org>
2022-05-30 17:53:50 -0700

    1) Retrofit 4-note songs for transposition via JS. 2) Update future feature text. 3) Update transpose.js functions for 4-note songs.

--- js/transpose.js
+++ js/transpose.js
@@ -4,9 +4,11 @@ function black_transpose() {
     let the_fs = document.getElementsByClassName("f")
     let the_gs = document.getElementsByClassName("g")
     let the_as = document.getElementsByClassName("a")
+    let the_cs = document.getElementsByClassName("c")
     let the_fsharps = document.getElementsByClassName("fsharp")
     let the_gsharps = document.getElementsByClassName("gsharp")
     let the_asharps = document.getElementsByClassName("asharp")
+    let the_csharps = document.getElementsByClassName("csharp")
 
     for (let i=0; i<the_fs.length; i++) {
         the_fs[i].src = "../../images/pngs/single_f_sharp.png"
@@ -19,6 +21,11 @@ function black_transpose() {
     for (let i=0; i<the_as.length; i++) {
         the_as[i].src = "../../images/pngs/single_a_sharp.png"
     }
+    
+    for (let i=0; i<the_cs.length; i++) {
+        the_cs[i].src = "../../images/pngs/single_c_sharp.png"
+    }
+    
     for (let i=0; i<the_fsharps.length; i++) {
         the_fsharps[i].src = "../../images/pngs/single_f_sharp.png"
     }
@@ -30,19 +37,26 @@ function black_transpose() {
     for (let i=0; i<the_asharps.length; i++) {
         the_asharps[i].src = "../../images/pngs/single_a_sharp.png"
     }
+    
+    for (let i=0; i<the_csharps.length; i++) {
+        the_csharps[i].src = "../../images/pngs/single_c_sharp.png"
+    }
 }
 
 function white_transpose() {
     let the_fs = document.getElementsByClassName("f")
     let the_gs = document.getElementsByClassName("g")
     let the_as = document.getElementsByClassName("a")
+    let the_cs = document.getElementsByClassName("c")
     let the_fsharps = document.getElementsByClassName("fsharp")
     let the_gsharps = document.getElementsByClassName("gsharp")
     let the_asharps = document.getElementsByClassName("asharp")
+    let the_csharps = document.getElementsByClassName("csharp")
 
     for (let i=0; i<the_fs.length; i++) {
         the_fs[i].src = "../../images/pngs/single_f.png"
     }
+    
     for (let i=0; i<the_gs.length; i++) {
         the_gs[i].src = "../../images/pngs/single_g.png"
     }
@@ -50,6 +64,11 @@ function white_transpose() {
     for (let i=0; i<the_as.length; i++) {
         the_as[i].src = "../../images/pngs/single_a.png"
     }
+    
+    for (let i=0; i<the_cs.length; i++) {
+        the_cs[i].src = "../../images/pngs/single_c.png"
+    }
+    
     for (let i=0; i<the_fsharps.length; i++) {
         the_fsharps[i].src = "../../images/pngs/single_f.png"
     }
@@ -60,5 +79,9 @@ function white_transpose() {
 
     for (let i=0; i<the_asharps.length; i++) {
         the_asharps[i].src = "../../images/pngs/single_a.png"
+    }
+    
+    for (let i=0; i<the_csharps.length; i++) {
+        the_csharps[i].src = "../../images/pngs/single_c.png"
     }
 }
