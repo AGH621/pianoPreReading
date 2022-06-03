944d4f41fa6599f30f6d205b01fb222ae2c2be52
Anne <anne@mfm.org>
2022-06-02 18:15:42 -0700

    Removed duplicate html directory

--- js/chord_view.js
+++ js/chord_view.js
@@ -1,6 +1,6 @@
-console.log("Transponse is running");
+console.log("Chord view is running")
 
-function black_transpose() {
+function white_chords() {
     let the_fs = document.getElementsByClassName("f")
     let the_gs = document.getElementsByClassName("g")
     let the_as = document.getElementsByClassName("a")
@@ -11,39 +11,39 @@ function black_transpose() {
     let the_csharps = document.getElementsByClassName("csharp")
 
     for (let i=0; i<the_fs.length; i++) {
-        the_fs[i].src = "../../images/pngs/single_f_sharp.png"
+        the_fs[i].src = "../../images/pngs/simple_wht_tonic.png"
     }
-
+    
     for (let i=0; i<the_gs.length; i++) {
-        the_gs[i].src = "../../images/pngs/single_g_sharp.png"
+        the_gs[i].src = "../../images/pngs/simple_wht_dom.png"
     }
-
+    
     for (let i=0; i<the_as.length; i++) {
-        the_as[i].src = "../../images/pngs/single_a_sharp.png"
+        the_as[i].src = "../../images/pngs/simple_wht_tonic.png"
     }
     
     for (let i=0; i<the_cs.length; i++) {
-        the_cs[i].src = "../../images/pngs/single_c_sharp.png"
+        the_cs[i].src = "../../images/pngs/simple_wht_dom.png"
     }
     
     for (let i=0; i<the_fsharps.length; i++) {
-        the_fsharps[i].src = "../../images/pngs/single_f_sharp.png"
+        the_fsharps[i].src = "../../images/pngs/simple_wht_tonic.png"
     }
-
+    
     for (let i=0; i<the_gsharps.length; i++) {
-        the_gsharps[i].src = "../../images/pngs/single_g_sharp.png"
+        the_gsharps[i].src = "../../images/pngs/simple_wht_dom.png"
     }
-
+    
     for (let i=0; i<the_asharps.length; i++) {
-        the_asharps[i].src = "../../images/pngs/single_a_sharp.png"
+        the_asharps[i].src = "../../images/pngs/simple_wht_tonic.png"
     }
     
     for (let i=0; i<the_csharps.length; i++) {
-        the_csharps[i].src = "../../images/pngs/single_c_sharp.png"
+        the_csharps[i].src = "../../images/pngs/simple_wht_dom.png"
     }
 }
 
-function white_transpose() {
+function black_chords() {
     let the_fs = document.getElementsByClassName("f")
     let the_gs = document.getElementsByClassName("g")
     let the_as = document.getElementsByClassName("a")
@@ -52,36 +52,36 @@ function white_transpose() {
     let the_gsharps = document.getElementsByClassName("gsharp")
     let the_asharps = document.getElementsByClassName("asharp")
     let the_csharps = document.getElementsByClassName("csharp")
-
+    
     for (let i=0; i<the_fs.length; i++) {
-        the_fs[i].src = "../../images/pngs/single_f.png"
+        the_fs[i].src = "../../images/pngs/simple_blk_tonic.png"
     }
     
     for (let i=0; i<the_gs.length; i++) {
-        the_gs[i].src = "../../images/pngs/single_g.png"
+        the_gs[i].src = "../../images/pngs/simple_blk_dom.png"
     }
-
+    
     for (let i=0; i<the_as.length; i++) {
-        the_as[i].src = "../../images/pngs/single_a.png"
+        the_as[i].src = "../../images/pngs/simple_blk_tonic.png"
     }
     
     for (let i=0; i<the_cs.length; i++) {
-        the_cs[i].src = "../../images/pngs/single_c.png"
+        the_cs[i].src = "../../images/pngs/simple_blk_dom.png"
     }
     
     for (let i=0; i<the_fsharps.length; i++) {
-        the_fsharps[i].src = "../../images/pngs/single_f.png"
+        the_fsharps[i].src = "../../images/pngs/simple_blk_tonic.png"
     }
-
+    
     for (let i=0; i<the_gsharps.length; i++) {
-        the_gsharps[i].src = "../../images/pngs/single_g.png"
+        the_gsharps[i].src = "../../images/pngs/simple_blk_dom.png"
     }
-
+    
     for (let i=0; i<the_asharps.length; i++) {
-        the_asharps[i].src = "../../images/pngs/single_a.png"
+        the_asharps[i].src = "../../images/pngs/simple_blk_tonic.png"
     }
     
     for (let i=0; i<the_csharps.length; i++) {
-        the_csharps[i].src = "../../images/pngs/single_c.png"
+        the_csharps[i].src = "../../images/pngs/simple_blk_dom.png"
     }
 }
