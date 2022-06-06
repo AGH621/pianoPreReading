5a8c9c76613238a6cbe0674d42806f346aee184d
Anne <anne@mfm.org>
2022-06-05 17:06:51 -0700

    Changed all 4-note songs to accommodate new JS chord view code.  Still need to write code to hide finger numbers in chord view.

--- js/chord_view.js
+++ js/chord_view.js
@@ -1,30 +1,60 @@
+/*
+Chord View
+Created By: Anne Hamill
+Date: 29 May 2022
+Description: Change the symbolic notation images from melody notes to chord diagrams.
+TODO:
+    1) Modify existing 3-note functions to hide finger numbers in chord view.
+    2) Create a single generalized function which can iterate through different classes and replace different images.
+*/
+
 console.log("Chord view is running")
-
-
-
-
 
 
 //Iterate through each one. Locate the melody note image.
 function white_3note_chords(){
-    let tonic_block = document.getElementsByClassName('tonic');
-    let dominant_block = document.getElementsByClassName('dominant');
+    let white_tonic_block = document.getElementsByClassName('tonic');
+    let white_dominant_block = document.getElementsByClassName('dominant');
     
-    for (let x=0; x<tonic_block.length; x++) {
-        i_img = tonic_block[x].querySelectorAll('img.pitch');
+    for (let x=0; x<white_tonic_block.length; x++) {
+        wi_img = white_tonic_block[x].querySelectorAll('img.pitch');
     
         //Change it to the tonic chord image.
-        for (let y=0; y<i_img.length; y++) {
-            i_img[y].src = "../../images/pngs/full_wht_tonic.png"    
+        for (let y=0; y<wi_img.length; y++) {
+            wi_img[y].src = "../../images/pngs/full_wht_tonic.png"    
         }   
     }
     
-    for (let x=0; x<dominant_block.length; x++) {
-        d_img = dominant_block[x].querySelectorAll('img.pitch');
+    for (let x=0; x<white_dominant_block.length; x++) {
+        wv_img = white_dominant_block[x].querySelectorAll('img.pitch');
+    
+        //Change it to the dominant chord image.
+        for (let y=0; y<wv_img.length; y++) {
+            wv_img[y].src = "../../images/pngs/full_wht_dom.png"    
+        }   
+    }
+}
+
+//Iterate through each one. Locate the melody note image.
+function black_3note_chords(){
+    let black_tonic_block = document.getElementsByClassName('tonic');
+    let black_dominant_block = document.getElementsByClassName('dominant');
+    
+    for (let x=0; x<black_tonic_block.length; x++) {
+        bi_img = black_tonic_block[x].querySelectorAll('img.pitch');
     
         //Change it to the tonic chord image.
-        for (let y=0; y<d_img.length; y++) {
-            d_img[y].src = "../../images/pngs/full_wht_dom.png"    
+        for (let y=0; y<bi_img.length; y++) {
+            bi_img[y].src = "../../images/pngs/full_blk_tonic.png"    
+        }   
+    }
+    
+    for (let x=0; x<black_dominant_block.length; x++) {
+        bv_img = black_dominant_block[x].querySelectorAll('img.pitch');
+    
+        //Change it to the dominant chord image.
+        for (let y=0; y<bv_img.length; y++) {
+            bv_img[y].src = "../../images/pngs/full_blk_dom.png"    
         }   
     }
 }
