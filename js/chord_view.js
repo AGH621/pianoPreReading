321d0b681b393f5b671ca1c51ac4fa50fdd89a3a
Anne <anne@mfm.org>
2022-06-06 21:13:52 -0700

    Improved the transpose.js script by using a loop and conditionals.  Could not get a switch statement to work. Put it on the to do list

--- js/chord_view.js
+++ js/chord_view.js
@@ -4,8 +4,7 @@ Created By: Anne Hamill
 Date: 29 May 2022
 Description: Change the symbolic notation images from melody notes to chord diagrams.
 TODO:
-    1) Modify existing 3-note functions to hide finger numbers in chord view.
-    2) Create a single generalized function which can iterate through different classes and replace different images.
+    1) Create a single generalized function which can iterate through different classes and replace different kinds of images.
 */
 
 console.log("Chord view is running")
@@ -128,95 +127,6 @@ function black_2note_chords() {
         }   
     }
 }
-
-
-function white_chords() {
-    let the_fs = document.getElementsByClassName("f")
-    let the_gs = document.getElementsByClassName("g")
-    let the_as = document.getElementsByClassName("a")
-    let the_cs = document.getElementsByClassName("c")
-    let the_fsharps = document.getElementsByClassName("fsharp")
-    let the_gsharps = document.getElementsByClassName("gsharp")
-    let the_asharps = document.getElementsByClassName("asharp")
-    let the_csharps = document.getElementsByClassName("csharp")
-    
-    for (let i=0; i<the_fs.length; i++) {
-        the_fs[i].src = "../../images/pngs/simple_wht_tonic.png"
-    }
-    
-    for (let i=0; i<the_gs.length; i++) {
-        the_gs[i].src = "../../images/pngs/simple_wht_dom.png"
-    }
-    
-    for (let i=0; i<the_as.length; i++) {
-        the_as[i].src = "../../images/pngs/simple_wht_tonic.png"
-    }
-    
-    for (let i=0; i<the_cs.length; i++) {
-        the_cs[i].src = "../../images/pngs/simple_wht_dom.png"
-    }
-    
-    for (let i=0; i<the_fsharps.length; i++) {
-        the_fsharps[i].src = "../../images/pngs/simple_wht_tonic.png"
-    }
-    
-    for (let i=0; i<the_gsharps.length; i++) {
-        the_gsharps[i].src = "../../images/pngs/simple_wht_dom.png"
-    }
-    
-    for (let i=0; i<the_asharps.length; i++) {
-        the_asharps[i].src = "../../images/pngs/simple_wht_tonic.png"
-    }
-    
-    for (let i=0; i<the_csharps.length; i++) {
-        the_csharps[i].src = "../../images/pngs/simple_wht_dom.png"
-    }
-}
-
-function black_chords() {
-    let the_fs = document.getElementsByClassName("f")
-    let the_gs = document.getElementsByClassName("g")
-    let the_as = document.getElementsByClassName("a")
-    let the_cs = document.getElementsByClassName("c")
-    let the_fsharps = document.getElementsByClassName("fsharp")
-    let the_gsharps = document.getElementsByClassName("gsharp")
-    let the_asharps = document.getElementsByClassName("asharp")
-    let the_csharps = document.getElementsByClassName("csharp")
-    
-    for (let i=0; i<the_fs.length; i++) {
-        the_fs[i].src = "../../images/pngs/simple_blk_tonic.png"
-    }
-    
-    for (let i=0; i<the_gs.length; i++) {
-        the_gs[i].src = "../../images/pngs/simple_blk_dom.png"
-    }
-    
-    for (let i=0; i<the_as.length; i++) {
-        the_as[i].src = "../../images/pngs/simple_blk_tonic.png"
-    }
-    
-    for (let i=0; i<the_cs.length; i++) {
-        the_cs[i].src = "../../images/pngs/simple_blk_dom.png"
-    }
-    
-    for (let i=0; i<the_fsharps.length; i++) {
-        the_fsharps[i].src = "../../images/pngs/simple_blk_tonic.png"
-    }
-    
-    for (let i=0; i<the_gsharps.length; i++) {
-        the_gsharps[i].src = "../../images/pngs/simple_blk_dom.png"
-    }
-    
-    for (let i=0; i<the_asharps.length; i++) {
-        the_asharps[i].src = "../../images/pngs/simple_blk_tonic.png"
-    }
-    
-    for (let i=0; i<the_csharps.length; i++) {
-        the_csharps[i].src = "../../images/pngs/simple_blk_dom.png"
-    }
-}
-
-
 
 
 
--- js/transpose.js
+++ js/transpose.js
@@ -1,78 +1,64 @@
+/*
+Transpose
+Created By: Anne Hamill
+Date: 29 May 2022
+Description: Change the symbolic notation images from white key melody to black key and vice versa.
+TODO:
+    1) Improve the two functions by figuring how to make the if/else if conditionals into switch statements.
+    2) Create a single generalized function which can iterate through different classes and replace different kinds of images.
+*/
+
+
+
 console.log("Transponse is running");
 
-let the_fs = document.getElementsByClassName("f")
-let the_gs = document.getElementsByClassName("g")
-let the_as = document.getElementsByClassName("a")
-let the_cs = document.getElementsByClassName("c")
-let the_fsharps = document.getElementsByClassName("fsharp")
-let the_gsharps = document.getElementsByClassName("gsharp")
-let the_asharps = document.getElementsByClassName("asharp")
-let the_csharps = document.getElementsByClassName("csharp")
+let pitches = document.getElementsByClassName('pitch')
 
 function black_transpose() {
-    for (let i=0; i<the_fs.length; i++) {
-        the_fs[i].src = "../../images/pngs/single_f_sharp.png"
-    }
-
-    for (let i=0; i<the_gs.length; i++) {
-        the_gs[i].src = "../../images/pngs/single_g_sharp.png"
-    }
-
-    for (let i=0; i<the_as.length; i++) {
-        the_as[i].src = "../../images/pngs/single_a_sharp.png"
-    }
+    for (let x=0; x<pitches.length; x++){
+        if (pitches[x].src.endsWith('f.png')){
+            console.log(pitches[x].src)
+            pitches[x].src = "../../images/pngs/single_f_sharp.png"
+        }
     
-    for (let i=0; i<the_cs.length; i++) {
-        the_cs[i].src = "../../images/pngs/single_c_sharp.png"
-    }
+        else if (pitches[x].src.endsWith('g.png')){
+            console.log(pitches[x].src)
+            pitches[x].src = "../../images/pngs/single_g_sharp.png"
+        }
     
-    for (let i=0; i<the_fsharps.length; i++) {
-        the_fsharps[i].src = "../../images/pngs/single_f_sharp.png"
-    }
-
-    for (let i=0; i<the_gsharps.length; i++) {
-        the_gsharps[i].src = "../../images/pngs/single_g_sharp.png"
-    }
-
-    for (let i=0; i<the_asharps.length; i++) {
-        the_asharps[i].src = "../../images/pngs/single_a_sharp.png"
-    }
+        else if (pitches[x].src.endsWith('a.png')){
+            console.log(pitches[x].src)
+            pitches[x].src = "../../images/pngs/single_a_sharp.png"
+        }
     
-    for (let i=0; i<the_csharps.length; i++) {
-        the_csharps[i].src = "../../images/pngs/single_c_sharp.png"
+        else if (pitches[x].src.endsWith('c.png')){
+            console.log(pitches[x].src)
+            pitches[x].src = "../../images/pngs/single_c_sharp.png"
+        }
     }
 }
 
 function white_transpose() {
-    for (let i=0; i<the_fs.length; i++) {
-        the_fs[i].src = "../../images/pngs/single_f.png"
-    }
+    for (let x=0; x<pitches.length; x++){
+        if (pitches[x].src.endsWith('f_sharp.png')){
+            console.log(pitches[x].src)
+            pitches[x].src = "../../images/pngs/single_f.png"
+        }
     
-    for (let i=0; i<the_gs.length; i++) {
-        the_gs[i].src = "../../images/pngs/single_g.png"
-    }
-
-    for (let i=0; i<the_as.length; i++) {
-        the_as[i].src = "../../images/pngs/single_a.png"
-    }
+        else if (pitches[x].src.endsWith('g_sharp.png')){
+            console.log(pitches[x].src)
+            pitches[x].src = "../../images/pngs/single_g.png"
+        }
     
-    for (let i=0; i<the_cs.length; i++) {
-        the_cs[i].src = "../../images/pngs/single_c.png"
-    }
+        else if (pitches[x].src.endsWith('a_sharp.png')){
+            console.log(pitches[x].src)
+            pitches[x].src = "../../images/pngs/single_a.png"
+        }
     
-    for (let i=0; i<the_fsharps.length; i++) {
-        the_fsharps[i].src = "../../images/pngs/single_f.png"
-    }
-
-    for (let i=0; i<the_gsharps.length; i++) {
-        the_gsharps[i].src = "../../images/pngs/single_g.png"
-    }
-
-    for (let i=0; i<the_asharps.length; i++) {
-        the_asharps[i].src = "../../images/pngs/single_a.png"
-    }
-    
-    for (let i=0; i<the_csharps.length; i++) {
-        the_csharps[i].src = "../../images/pngs/single_c.png"
+        else if (pitches[x].src.endsWith('c_sharp.png')){
+            console.log(pitches[x].src)
+            pitches[x].src = "../../images/pngs/single_c.png"
+        }
     }
 }
+
