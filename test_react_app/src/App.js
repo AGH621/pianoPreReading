29d096539a756e069890ce16e402ffce4f4370cc
Anne <aghamill@sbcglobal.net>
2022-06-15 20:52:25 -0700

    Built a test React app using notes from au claire de la lune.  Imports (all css and images) have to be in the app's src directory.  Need to research a better way to import.  Google fonts do not load. Do not know why.

--- test_react_app/src/App.js
+++ test_react_app/src/App.js
@@ -1,13 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+import './note_style.css';
+
+import an_f from './single_f.png';
+import a_g from './single_g.png';
+import an_a from './single_a.png';
+
+/*
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          To be or not to be, that is the question
         </p>
         <a
           className="App-link"
@@ -20,6 +27,40 @@ function App() {
       </header>
     </div>
   );
+}*/
+
+/*
+change "duple" to "duple_meter" same for "triple" to "triple_meter"
+*/
+
+function TestNote() {
+    return (
+        <section className="duple">
+            <div className="note">
+                <p className="tonic">I
+                <br />
+                <img className="pitch f" src={an_f} />
+                <br />
+                Do</p>
+            </div>
+        
+            <div className="note">
+                <p className="dominant">V7
+                <br />
+                <img className="pitch g" src={a_g} />
+                <br />
+                Re</p>
+            </div>
+        
+            <div className="note">
+                <p className="tonic">I
+                <br />
+                <img className="pitch a" src={an_a} />
+                <br />
+                Mi</p>
+            </div>
+        </section>
+    )
 }
 
-export default App;
+export default TestNote;
