92aed484aa8c2e52a722c2de9746509a0b9ea53a
Anne <aghamill@sbcglobal.net>
2022-07-09 19:51:19 -0700

    1) Copied images into React directory tree. 2) Modified the oneNote function to include all the note components 3) Wrote a new function to wrap each note in the html tags which provide layout styling.

--- test_react_app/src/routes/song_page.js
+++ test_react_app/src/routes/song_page.js
@@ -13,44 +13,53 @@ import { getTitle } from "../data/song_builder";
 import SingleA from "./images/single_a.png";
 import SingleG from "./images/single_g.png";
 import SingleF from "./images/single_f.png";
+import LfFour from "./images/L4.png";
+import LfThree from "./images/L3.png";
+import LfTwo from "./images/L2.png";
 
 
 
-function oneNote(a_song) {
-    let params = useParams();
-    //let the_song = getTitle(params.songTitle);
-
-    let the_notes = [];
+function oneNote(a_note) {
     let the_pix = [];
-    
-    for (let n=0; n<a_song.notes.length; n++) {
-        console.log(a_song.notes[n].pitch)
-       
-        if (a_song.notes[n].pitch === 'Mi') {
-            the_pix.push(<img className="pitch mi" src={SingleA} />);
-            the_pix.push(<span className="chord">I</span>);
-            the_pix.push(<br />);
-            the_pix.push(a_song.notes[n].lyric);
-            
-        }
-        else if (a_song.notes[n].pitch === 'Re') {
-            the_pix.push(<img className="pitch re" src={SingleG} />);
-            the_pix.push(<span className="chord">V7</span>);
-            the_pix.push(<br />);
-            the_pix.push(a_song.notes[n].lyric);
-        }
-        else if (a_song.notes[n].pitch === 'Do') {
-            the_pix.push(<img className="pitch do" src={SingleF} />);
-            the_pix.push(<span className="chord">I</span>);
-            the_pix.push(<br />);
-            the_pix.push(a_song.notes[n].lyric);
-        }
+
+    if (a_note === 'Mi') {
+        the_pix.push(<img className="fingerings" src={LfTwo} />)
+        the_pix.push(<span className="chord">I</span>)
+        the_pix.push(<br />)
+        the_pix.push(<img className="pitch mi" src={SingleA} />)
+    }
+    else if (a_note === 'Re') {
+        the_pix.push(<img className="fingerings" src={LfThree} />)
+        the_pix.push(<span className="chord">V7</span>)
+        the_pix.push(<br />)
+        the_pix.push(<img className="pitch re" src={SingleG} />)
+    }
+    else if (a_note === 'Do') {
+        the_pix.push(<img className="fingerings" src={LfFour} />)
+        the_pix.push(<span className="chord">I</span>)
+        the_pix.push(<br />)
+        the_pix.push(<img className="pitch do" src={SingleF} />)
     }
     
     return the_pix
 }
 
-
+function writeSong(a_song) {
+    let component_list = []
+    
+    for (let n=0; n<a_song.notes.length; n++) {
+        component_list.push(
+        <div class="note">
+            <p class="tonic">
+            {oneNote(a_song.notes[n].pitch)}
+            <br />
+            {a_song.notes[n].lyric}
+            </p>
+        </div>
+        )
+    }
+    return component_list
+}
 
 
 export default function SongPage() {
@@ -60,11 +69,10 @@ export default function SongPage() {
     return (
         <main style={{ padding: "1rem" }}>
             <h1 className = "title">{params.songTitle}</h1>
-            <p> {the_song.score_data.score_def_timestamp}</p>
-            <section className=the_song.score_data.meter>
-                {oneNote(the_song)}
+        
+            <section className={the_song.score_data.meter}>
+                {writeSong(the_song)}
             </section>
-            
         </main>
     );
 }
