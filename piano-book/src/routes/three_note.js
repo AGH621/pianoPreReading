3cb3709391bc84d6410716681063f8e2faec01bf
Anne <aghamill@sbcglobal.net>
2022-08-28 14:44:34 -0700

    Trying to debug appDrawer

--- piano-book/src/routes/three_note.js
+++ piano-book/src/routes/three_note.js
@@ -34,8 +34,47 @@ import appDrawer from '../components/app_drawer';
 import teacherText from '../data/home_page/home_teacher.txt';
 import studentText from '../data/home_page/home_student.txt';
 
+const drawerWidth = 240;
 
-export default function ThreeNote() {
+/*
+interface Props {
+  *
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   *
+  window?: () => Window;
+}*/
+
+
+export default function ThreeNote(props: Props) {
+  /*
+  const { window } = props;
+    const [mobileOpen, setMobileOpen] = React.useState(false);
+  
+    const handleDrawerToggle = () => {
+      setMobileOpen(!mobileOpen);
+    };
+  
+    const drawer = (
+      <div>
+        <Typography variant="h3" style={{padding: "1rem" }}>
+            Song List
+        </Typography>
+        <Divider />
+        <List>
+          {['Song 1', 'Song 2', 'Song 3', 'Song 4'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton to={`/`} key={text}>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </div>
+    );
+  
+    const container = window !== undefined ? () => window().document.body : undefined;*/
+  
 
   return (
     <Grid position="relative">  
@@ -44,6 +83,49 @@ export default function ThreeNote() {
       
       {appDrawer}
       
+     {/*
+      <Box
+             component="nav"
+             sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+           >
+             
+             <Drawer
+             PaperProps={{
+                 sx: {
+                     marginTop: "3em"
+                 }
+               }}
+               container={container}
+               variant="temporary"
+               open={mobileOpen}
+               onClose={handleDrawerToggle}
+               ModalProps={{
+                 keepMounted: true, // Better open performance on mobile.
+               }}
+               sx={{
+                 display: { xs: 'block', sm: 'none' },
+                 '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+               }}
+             >
+               {drawer}
+             </Drawer>
+             <Drawer
+               PaperProps={{
+                   sx: {
+                       marginTop: "3em",
+                   }
+                 }}
+               variant="permanent"
+               sx={{
+                 display: { xs: 'none', sm: 'block' },
+                 '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+               }}
+               open
+             >
+               {drawer}
+             </Drawer>
+           </Box>*/}
+     
       <Box
         component="main"
         sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` }}}
