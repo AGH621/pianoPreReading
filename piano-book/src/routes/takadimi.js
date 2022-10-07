1dd9ac52dfe2c1d07a599a74eae5f8139b454120
Anne <aghamill@sbcglobal.net>
2022-10-06 20:57:49 -0700

    Test commit for debugging

--- piano-book/src/routes/takadimi.js
+++ piano-book/src/routes/takadimi.js
@@ -1,60 +1,32 @@
 
-import * as React from 'react';
+import React from 'react';
+import { Outlet, Link } from "react-router-dom";
 import { Typography,
          Grid,
          Box,
          Paper,
          ThemeProvider } from '@mui/material';
-         
-import { Outlet, Link } from "react-router-dom";
+
+import { theme } from '../siteTheme.js'
 
 import Footer from '../components/footer';
 import paperGridRow from '../components/app_papergrid';
 import returnTopFab from '../components/app_returntop';
-import { theme } from '../siteTheme.js'
+
 
 import lesson1 from '../data/takadimi_files/Lesson_1.pdf';
 import lesson2 from '../data/takadimi_files/Lesson_2.pdf';
 
 export default function Takadimi() {
-    /*
-    return (
-            <Box>
-                <Typography variant="h1" sx={{margin: "1em"}}>Ta-Ka-Di-Mi Rhythm Lessons</Typography>
-            
-          <object
-                type="application/pdf"
-            data={lesson1}
-            target="_blank"
-                width="600"
-                height="700"
-              >
-              </object>
-            
-            <Grid sx={{ flexGrow: 1,
-                        marginTop: "1em"}} container spacing={2}>
-              <Grid item xs={12}>
-                {[1, 2, 3, 4, 5, 6].map((value) => (
-                    <Box>
-                  <Typography variant="h3" sx={{margin: "1em"}}>Lesson {value}</Typography>
-                    {paperGridRow()}
-                    </Box>
-                ))}
-              </Grid>
-              </Grid>
-                
-              
-                
-                {returnTopFab()}
-              <Footer />      
-              <Outlet />
-              </Box>      
-        );*/
+ 
     
     return (
             <ThemeProvider>
-                <Box>
-                    Rhythm Lessons
+                <Paper sx={{backgroundColor: "backgrounds.nav"}}>
+                    <Box sx={{padding: "5em",
+                          height: "750px",}}>
+                    
+                    
                     <h3>Click on below link to open 
                         PDF file in new tab</h3>
                     <a href={lesson1} target="_blank" 
@@ -65,8 +37,9 @@ export default function Takadimi() {
                         rel="noreferrer">
                         Lesson 2
                     </a>
+                
                 </Box>
-        
+        </Paper>
                 <Footer />
         
                 <Outlet />
@@ -74,11 +47,4 @@ export default function Takadimi() {
         );
 }
 
-/*
-<object
-      type="application/pdf"
-  data={lesson1Page1}
-      width="600"
-      height="700"
-    >
-    </object>*/
+
